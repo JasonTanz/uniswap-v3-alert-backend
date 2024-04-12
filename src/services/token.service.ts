5a2b69e96e0244f945313026a0f7cf5a6df6cc11
Jasontanz <zisheng0101@gmail.com>
2024-04-12 15:47:51 +0800

    feat: token moudle

---
+++ src/services/token.service.ts
@@ -0,0 +1,32 @@
+import logger from '../config/logger';
+import Token from '../models/token.model';
+
+//--------------- Create Token
+const create = async (payload: any): Promise<any> => {
+  try {
+    const newToken = new Token({
+      ...payload,
+    });
+    await newToken.save();
+    logger.info('Token created successfully:', newToken);
+    return [null, newToken];
+  } catch (error) {
+    logger.error('Error creating pool:', error);
+    return [error, null];
+  }
+};
+
+//--------------- Get Token by address
+const getByAddress = async (address: string): Promise<any> => {
+  try {
+    const token = await Token.findOne({ address }).exec();
+    logger.info('Fetch token by address:', address);
+    return [null, token];
+  } catch (error) {
+    logger.error('Error fetching token by address:', error);
+
+    return [error, null];
+  }
+};
+
+export { create, getByAddress };
